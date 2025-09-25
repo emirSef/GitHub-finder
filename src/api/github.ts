e551200d1ed5598aecbd3bb6b3c7e3a52f77e88e
Emir <emir.seferovic33@gmail.com>
2025-09-25 15:30:32 +0200

    first commit

---
+++ src/api/github.ts
@@ -0,0 +1,18 @@
+export const fetchGithubUser = async (username: string) => {
+  const res = await fetch(
+    `${import.meta.env.VITE_GITHUB_API_URL}/users/${username}`
+  );
+  if (!res) throw new Error("User not found");
+
+  const data = await res.json();
+  return data;
+};
+export const searchGithubUser = async (query: string) => {
+  const res = await fetch(
+    `${import.meta.env.VITE_GITHUB_API_URL}/search/users?q=${query}`
+  );
+  if (!res) throw new Error("User not found");
+
+  const data = await res.json();
+  return data.items;
+};
